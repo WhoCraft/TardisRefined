f765c5d46bb1136d13074c49f62b6fceac43bde3
Craig <sandedshoes@gmail.com>
2022-12-14 14:37:21 +0000

    - Shell & Console blocks can no longer be broken

--- common/src/main/java/whocraft/tardis_refined/common/util/MiscHelper.java
+++ common/src/main/java/whocraft/tardis_refined/common/util/MiscHelper.java
@@ -3,13 +3,22 @@ package whocraft.tardis_refined.common.util;
 import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.block.state.BlockState;
 import whocraft.tardis_refined.TardisRefined;
+import whocraft.tardis_refined.common.block.console.GlobalConsoleBlock;
+import whocraft.tardis_refined.common.block.shell.GlobalShellBlock;
 
 public class MiscHelper {
 
     @ExpectPlatform
     public static Packet<?> spawnPacket(Entity entity) {
         throw new RuntimeException(TardisRefined.PLATFORM_ERROR);
+    }
+
+    public static boolean shouldCancelBreaking(BlockState blockState) {
+        boolean s = blockState.getBlock() instanceof GlobalConsoleBlock || blockState.getBlock() instanceof GlobalShellBlock;
+        System.out.println(s);
+        return s;
     }
 
 }
--- forge/src/main/java/whocraft/tardis_refined/forge/CommonBus.java
+++ forge/src/main/java/whocraft/tardis_refined/forge/CommonBus.java
@@ -3,10 +3,13 @@ package whocraft.tardis_refined.forge;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import whocraft.tardis_refined.TardisRefined;
+import whocraft.tardis_refined.common.block.console.GlobalConsoleBlock;
 import whocraft.tardis_refined.common.dimension.DelayedTeleportData;
+import whocraft.tardis_refined.common.util.MiscHelper;
 
 @Mod.EventBusSubscriber(modid = TardisRefined.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CommonBus {
@@ -19,6 +22,11 @@ public class CommonBus {
                 DelayedTeleportData.tick(serverLevel);
             }
         }
+    }
+
+    @SubscribeEvent
+    public static void onBlockBreak(BlockEvent.BreakEvent event){
+        event.setCanceled(MiscHelper.shouldCancelBreaking(event.getState()));
     }
 
 }
