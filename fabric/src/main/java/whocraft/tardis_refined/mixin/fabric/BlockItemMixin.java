de619081feb07a87cbc8cc3a38437d55adfd33fa
Craig <sandedshoes@gmail.com>
2022-12-21 23:53:49 +0000

    Blocks can no longer be placed on Fabric within ProtectedZones

--- fabric/src/main/java/whocraft/tardis_refined/mixin/fabric/BlockItemMixin.java
+++ fabric/src/main/java/whocraft/tardis_refined/mixin/fabric/BlockItemMixin.java
@@ -1,26 +1,28 @@
 package whocraft.tardis_refined.mixin.fabric;
 
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.Level;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.context.UseOnContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import whocraft.tardis_refined.common.util.MiscHelper;
 
-@Mixin(BlockPlaceContext.class)
+@Mixin(BlockItem.class)
 public class BlockItemMixin {
 
 
-    @Inject(at = @At("HEAD"), method = "canPlace()Z", cancellable = true)
-    private void place(CallbackInfoReturnable<Boolean> cir) {
-        BlockPlaceContext blockPlaceContext = (BlockPlaceContext) (Object) this;
-
-        Level level = blockPlaceContext.getLevel();
-        if (MiscHelper.shouldStopItem(level, blockPlaceContext.getPlayer(), blockPlaceContext.getClickedPos())) {
-            level.removeBlock(blockPlaceContext.getClickedPos(), true);
-            cir.setReturnValue(false);
+    @Inject(method = "useOn(Lnet/minecraft/world/item/context/UseOnContext;)Lnet/minecraft/world/InteractionResult;", at = @At("HEAD"), cancellable = true)
+    private void restrict(UseOnContext useOnContext, CallbackInfoReturnable<InteractionResult> cir) {
+        if (MiscHelper.shouldStopItem(useOnContext.getLevel(), useOnContext.getPlayer(), useOnContext.getClickedPos())) {
+            if (useOnContext.getPlayer() instanceof ServerPlayer serverPlayer) {
+                serverPlayer.inventoryMenu.sendAllDataToRemote();
+            }
+            cir.setReturnValue(InteractionResult.PASS);
         }
     }
+
 
 }
