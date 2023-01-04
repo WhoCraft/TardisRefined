2df6e7b878931396827b2255579b28a20a9d65ce
The Judge <53906078+thejudge156@users.noreply.github.com>
2023-01-03 19:46:48 -0800

    Better locking

--- common/src/main/java/whocraft/tardis_refined/common/blockentity/door/GlobalDoorBlockEntity.java
+++ common/src/main/java/whocraft/tardis_refined/common/blockentity/door/GlobalDoorBlockEntity.java
@@ -1,12 +1,18 @@
 package whocraft.tardis_refined.common.blockentity.door;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import whocraft.tardis_refined.common.block.door.GlobalDoorBlock;
 import whocraft.tardis_refined.common.capability.TardisLevelOperator;
+import whocraft.tardis_refined.common.items.KeyItem;
 import whocraft.tardis_refined.registry.BlockEntityRegistry;
 
 import java.util.Optional;
@@ -18,7 +24,7 @@ public class GlobalDoorBlockEntity extends AbstractEntityBlockDoor {
     }
 
 
-    public void onRightClick(BlockState blockState, ITardisInternalDoor door) {
+    public void onRightClick(BlockState blockState, ITardisInternalDoor door, Player player) {
         if (getLevel() instanceof ServerLevel serverLevel) {
 
             // we know that in this instance the serverlevel has a capability.
@@ -26,7 +32,17 @@ public class GlobalDoorBlockEntity extends AbstractEntityBlockDoor {
                 if (cap.getInternalDoor() != door) {
                     cap.setInternalDoor(door);
                 }
-                if (!cap.getControlManager().isInFlight()) {
+                ItemStack stack = player.getItemInHand(InteractionHand.MAIN_HAND);
+                ResourceKey<Level> dimension = level.dimension();
+
+                boolean validKey = KeyItem.keychainContains(stack, dimension);
+                if(validKey) {
+                    cap.getExteriorManager().setLocked(!door.locked());
+                    door.setLocked(!door.locked());
+                    cap.setDoorClosed(true);
+                    return;
+                }
+                if (!cap.getControlManager().isInFlight() && !door.locked()) {
                     cap.setDoorClosed(blockState.getValue(GlobalDoorBlock.OPEN));
                 }
             });
--- common/src/main/java/whocraft/tardis_refined/common/tardis/control/ship/ToggleDoorControl.java
+++ common/src/main/java/whocraft/tardis_refined/common/tardis/control/ship/ToggleDoorControl.java
@@ -1,5 +1,7 @@
 package whocraft.tardis_refined.common.tardis.control.ship;
 
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
 import whocraft.tardis_refined.common.block.door.GlobalDoorBlock;
 import whocraft.tardis_refined.common.capability.TardisLevelOperator;
@@ -9,11 +11,17 @@ import whocraft.tardis_refined.common.tardis.control.IControl;
 public class ToggleDoorControl implements IControl {
     @Override
     public void onRightClick(TardisLevelOperator operator, ControlEntity controlEntity, Player player) {
+        if(operator.getExteriorManager().locked()) {
+            controlEntity.level.playSound(null, operator.getInternalDoor().getDoorPosition(), SoundEvents.ANVIL_PLACE, SoundSource.AMBIENT, 1f, 1f);
+            return;
+        }
         operator.setDoorClosed(operator.getLevel().getBlockState(operator.getInternalDoor().getDoorPosition()).getValue(GlobalDoorBlock.OPEN));
     }
 
     @Override
     public void onLeftClick(TardisLevelOperator operator, ControlEntity controlEntity, Player player) {
-
+        operator.getInternalDoor().setLocked(!operator.getExteriorManager().locked());
+        operator.getExteriorManager().setLocked(!operator.getExteriorManager().locked());
+        operator.setDoorClosed(true);
     }
 }
