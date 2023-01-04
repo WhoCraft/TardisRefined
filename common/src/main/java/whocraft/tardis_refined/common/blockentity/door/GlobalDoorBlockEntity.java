29f7d1438e5342dafa5ea8f5b6f32829bba08748
The Judge <53906078+thejudge156@users.noreply.github.com>
2023-01-03 20:00:35 -0800

    Lock without key for interior door

--- common/src/main/java/whocraft/tardis_refined/common/blockentity/door/GlobalDoorBlockEntity.java
+++ common/src/main/java/whocraft/tardis_refined/common/blockentity/door/GlobalDoorBlockEntity.java
@@ -1,18 +1,12 @@
 package whocraft.tardis_refined.common.blockentity.door;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import whocraft.tardis_refined.common.block.door.GlobalDoorBlock;
 import whocraft.tardis_refined.common.capability.TardisLevelOperator;
-import whocraft.tardis_refined.common.items.KeyItem;
 import whocraft.tardis_refined.registry.BlockEntityRegistry;
 
 import java.util.Optional;
@@ -32,11 +26,7 @@ public class GlobalDoorBlockEntity extends AbstractEntityBlockDoor {
                 if (cap.getInternalDoor() != door) {
                     cap.setInternalDoor(door);
                 }
-                ItemStack stack = player.getItemInHand(InteractionHand.MAIN_HAND);
-                ResourceKey<Level> dimension = level.dimension();
-
-                boolean validKey = KeyItem.keychainContains(stack, dimension);
-                if(validKey) {
+                if(player.isCrouching()) {
                     cap.getExteriorManager().setLocked(!door.locked());
                     door.setLocked(!door.locked());
                     cap.setDoorClosed(true);
