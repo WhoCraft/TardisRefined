fbea3508dd716aab452afb35f71b8e140d130aa8
The Judge <53906078+thejudge156@users.noreply.github.com>
2023-01-06 17:26:27 -0800

    Fix anims being super fast

--- fabric/src/main/java/whocraft/tardis_refined/fabric/events/ModEvents.java
+++ fabric/src/main/java/whocraft/tardis_refined/fabric/events/ModEvents.java
@@ -4,28 +4,12 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.core.Registry;
-import net.minecraft.data.BuiltinRegistries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
-import qouteall.imm_ptl.core.portal.Portal;
-import qouteall.imm_ptl.core.portal.PortalManipulation;
-import qouteall.q_misc_util.my_util.DQuaternion;
-import whocraft.tardis_refined.TardisRefined;
-import whocraft.tardis_refined.api.event.TardisEvents;
 import whocraft.tardis_refined.client.TardisClientData;
 import whocraft.tardis_refined.common.capability.TardisLevelOperator;
 import whocraft.tardis_refined.common.dimension.DelayedTeleportData;
 import whocraft.tardis_refined.common.dimension.fabric.DimensionHandlerImpl;
-import whocraft.tardis_refined.common.tardis.IExteriorShell;
 import whocraft.tardis_refined.common.util.MiscHelper;
 import whocraft.tardis_refined.registry.DimensionTypes;
 
@@ -54,7 +38,12 @@ public class ModEvents {
 
     public static void addClientEvents() {
         ClientTickEvents.START_WORLD_TICK.register(world -> {
-            TardisClientData.getAllEntries().forEach((levelResourceKey, tardisClientData) -> tardisClientData.tickClientside());
+            TardisClientData.getAllEntries().forEach((levelResourceKey, tardisClientData) -> {
+                if(world.dimension() != levelResourceKey) {
+                    return;
+                }
+                tardisClientData.tickClientside();
+            });
 
             if (Minecraft.getInstance().level == null) {
                 TardisClientData.clearAll();
