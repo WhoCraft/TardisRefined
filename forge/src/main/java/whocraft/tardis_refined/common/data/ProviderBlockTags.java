8eeffab9f2196009405b291b2c25eb013ad829d4
Clayton Trewben <34910888+CommandrMoose@users.noreply.github.com>
2023-04-18 22:27:57 +1000

    WitherImmune and Addtional BOTI

--- forge/src/main/java/whocraft/tardis_refined/common/data/ProviderBlockTags.java
+++ forge/src/main/java/whocraft/tardis_refined/common/data/ProviderBlockTags.java
@@ -41,8 +41,10 @@ public class ProviderBlockTags extends BlockTagsProvider {
             }
         }
 
-        tag(BlockTags.DRAGON_IMMUNE).add(BlockRegistry.GLOBAL_SHELL_BLOCK.get());
         tag(BlockTags.DRAGON_IMMUNE).add(BlockRegistry.ROOT_SHELL_BLOCK.get());
+        tag(BlockTags.DRAGON_IMMUNE).add(BlockRegistry.GLOBAL_SHELL_BLOCK.get());
+        tag(BlockTags.WITHER_IMMUNE).add(BlockRegistry.ROOT_SHELL_BLOCK.get());
+        tag(BlockTags.WITHER_IMMUNE).add(BlockRegistry.GLOBAL_SHELL_BLOCK.get());
 
         tag(BlockTags.MINEABLE_WITH_PICKAXE).add(BlockRegistry.CONSOLE_CONFIGURATION_BLOCK.get());
         tag(BlockTags.MINEABLE_WITH_PICKAXE).add(BlockRegistry.LANDING_PAD.get());
