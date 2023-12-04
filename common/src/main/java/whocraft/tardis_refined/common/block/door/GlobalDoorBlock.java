a875c3399e7dd34af8386df8502bb544e5f2428c
The Judge <colepatirckroberts@gmail.com>
2023-12-04 10:27:02 -0800

    Can't walk through a portal, fixed

--- common/src/main/java/whocraft/tardis_refined/common/block/door/GlobalDoorBlock.java
+++ common/src/main/java/whocraft/tardis_refined/common/block/door/GlobalDoorBlock.java
@@ -96,10 +96,10 @@ public class GlobalDoorBlock extends InternalDoorBlock{
     }
 
     static {
-        NORTH_AABB = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 3.0D);
-        SOUTH_AABB = Block.box(0.0D, 0.0D, 13.0D, 16.0D, 16.0D, 16.0D);
-        EAST_AABB= Block.box(13.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
-        WEST_AABB = Block.box(0.0D, 0.0D, 0.0D, 3.0D, 16.0D, 16.0D);
+        NORTH_AABB = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 1.0D);
+        SOUTH_AABB = Block.box(0.0D, 0.0D, 15.0D, 16.0D, 16.0D, 16.0D);
+        EAST_AABB= Block.box(15.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+        WEST_AABB = Block.box(0.0D, 0.0D, 0.0D, 1.0D, 16.0D, 16.0D);
     }
 
 }
