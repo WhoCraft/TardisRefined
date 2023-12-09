e473176ba386237bb16a7d694bd0cad09d8ebebb
50ap5ud5 <50ap5ud5125@gmail.com>
2023-12-10 02:53:36 +1100

    Make TardisClientData use a dummy Pattern on game launch, fixes Neoforge crash when TardisClientData is ticked on game load when registries aren't populated yet

--- common/src/main/java/whocraft/tardis_refined/client/TardisClientData.java
+++ common/src/main/java/whocraft/tardis_refined/client/TardisClientData.java
@@ -43,13 +43,13 @@ public class TardisClientData {
     private boolean isOnCooldown = false;
     private float flightShakeScale = 0;
     private ResourceLocation shellTheme = ShellTheme.FACTORY.getId();
-    private ShellPattern shellPattern = safeGrabPattern();
+    private ShellPattern shellPattern = ShellPatterns.DEFAULT;
 
-    private ShellPattern safeGrabPattern() {
-        if (ShellPatterns.getRegistry().isEmpty())
-            return ShellPatterns.getPatternsForThemeDefault(shellTheme).get(0);
-        return ShellPatterns.getPatternsForTheme(shellTheme).get(0);
-    }
+//    private ShellPattern safeGrabPattern() {
+//        if (ShellPatterns.getRegistry().isEmpty())
+//            return ShellPatterns.getPatternsForThemeDefault(shellTheme).get(0);
+//        return ShellPatterns.getPatternsForTheme(shellTheme).get(0);
+//    }
 
     public TardisClientData setShellPattern(ShellPattern shellPattern) {
         this.shellPattern = shellPattern;
