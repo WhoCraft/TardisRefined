df9ca004b0c0f51e105c861d32da327538c07d95
Craig (Jeryn) <sandedshoes@gmail.com>
2024-03-07 21:29:23 +0000

    Dying specific Items (#201)

--- fabric/src/main/java/whocraft/tardis_refined/fabric/events/ModEvents.java
+++ fabric/src/main/java/whocraft/tardis_refined/fabric/events/ModEvents.java
@@ -1,11 +1,13 @@
 package whocraft.tardis_refined.fabric.events;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
+import whocraft.tardis_refined.client.TRItemColouring;
 import whocraft.tardis_refined.client.TardisClientData;
 import whocraft.tardis_refined.command.TardisRefinedCommand;
 import whocraft.tardis_refined.common.capability.TardisLevelOperator;
@@ -13,6 +15,7 @@ import whocraft.tardis_refined.common.dimension.TardisTeleportData;
 import whocraft.tardis_refined.common.dimension.fabric.DimensionHandlerImpl;
 import whocraft.tardis_refined.common.util.MiscHelper;
 import whocraft.tardis_refined.registry.DimensionTypes;
+import whocraft.tardis_refined.registry.ItemRegistry;
 
 import static net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents.END_WORLD_TICK;
 import static net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents.START_WORLD_TICK;
@@ -40,6 +43,7 @@ public class ModEvents {
 
     public static void addClientEvents() {
         ClientTickEvents.START_CLIENT_TICK.register(TardisClientData::tickClientData);
+        ColorProviderRegistry.ITEM.register(TRItemColouring.SCREWDRIVER_COLORS, ItemRegistry.SCREWDRIVER.get());
     }
 
 
