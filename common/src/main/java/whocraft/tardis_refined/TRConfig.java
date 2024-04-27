ef6b88bb9c98c551debf55bb65c2a7b86fd6d538
Clayton T. <34910888+CommandrMoose@users.noreply.github.com>
2024-04-28 08:52:28 +1000

    IP config disabled by false.

--- common/src/main/java/whocraft/tardis_refined/TRConfig.java
+++ common/src/main/java/whocraft/tardis_refined/TRConfig.java
@@ -53,7 +53,7 @@ public class TRConfig {
 
         public Common(ModConfigSpec.Builder builder) {
             builder.push("compatibility");
-            COMPATIBILITY_IP = builder.comment("Toggle Immersive Portals compatibility").translation(ModMessages.CONFIG_IP_COMPAT).define("immersive_portals", true);
+            COMPATIBILITY_IP = builder.comment("Toggle Immersive Portals compatibility (TR 2.0+)").translation(ModMessages.CONFIG_IP_COMPAT).define("immersive_portals_version_two", false);
             builder.pop();
         }
 
