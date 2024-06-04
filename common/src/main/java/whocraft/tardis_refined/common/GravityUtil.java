284c939c6daab54a0c6b98ccc554993b640f5e32
Craig <sandedshoes@gmail.com>
2024-06-04 23:24:48 +0100

    FPS Fix

--- common/src/main/java/whocraft/tardis_refined/common/GravityUtil.java
+++ common/src/main/java/whocraft/tardis_refined/common/GravityUtil.java
@@ -14,19 +14,22 @@ import static net.minecraft.core.BlockPos.betweenClosed;
 
 public class GravityUtil {
 
-
     private static final int MAX_Y = 30; // The most a shaft can carry a player up
     private static final double ACCELERATION = 0.2;
-    public static boolean isInAntiGrav(Player playerBox, AABB box, Level level) {
-//        if(level.dimensionTypeId() != TRDimensionTypes.TARDIS) return false; //Allow sound to play no matter what dimension the block is placed in
+    private static final RandomSource randomSource = RandomSource.create();
+
+    public static boolean isInAntiGrav(Player player, AABB box, Level level) {
+        if (level.dimensionTypeId() != TRDimensionTypes.TARDIS) return false;
+
+        BlockPos.MutableBlockPos mutablePos = new BlockPos.MutableBlockPos();
         for (BlockPos pos : betweenClosed(new BlockPos((int) box.maxX, (int) box.maxY, (int) box.maxZ), new BlockPos((int) box.minX, (int) box.minY, (int) box.minZ))) {
             BlockState blockState = level.getBlockState(pos);
             if (blockState.getBlock() instanceof AntiGravityBlock) {
                 int space = blockState.getValue(AntiGravityBlock.SPACE);
-                if (space > 0 && level.getBlockState(pos.above()).isAir()) {
-                    AABB myGravBox = GravityUtil.createGravityBoxFromLevel(level, pos, space);
-                    spawnParticlesWithinAABB(level, myGravBox);
-                    if (myGravBox.intersects(playerBox.getBoundingBox()) && playerBox.blockPosition().getY() >= pos.getY()) {
+                if (space > 0 && level.getBlockState(mutablePos.set(pos).move(0, 1, 0)).isAir()) {
+                    AABB myGravBox = createGravityBoxFromLevel(pos, space);
+                    if (myGravBox.intersects(player.getBoundingBox()) && player.blockPosition().getY() >= pos.getY()) {
+                        spawnParticlesWithinAABB(level, myGravBox);
                         return true;
                     }
                 }
@@ -35,9 +38,7 @@ public class GravityUtil {
         return false;
     }
 
-
     private static void spawnParticlesWithinAABB(Level level, AABB box) {
-        RandomSource randomSource = level.random;
         for (int i = 0; i < 2; i++) {
             double x = box.minX + (box.maxX - box.minX) * randomSource.nextDouble();
             double y = box.minY + (box.maxY - box.minY) * randomSource.nextDouble();
@@ -46,26 +47,16 @@ public class GravityUtil {
         }
     }
 
-
-    public static AABB createGravityBoxFromLevel(Level level, BlockPos blockPos, int range) {
-        if (level != null) {
-            return new AABB(blockPos).inflate(range, MAX_Y, range);
-        }
-        return null;
-
+    public static AABB createGravityBoxFromLevel(BlockPos blockPos, int range) {
+        return new AABB(blockPos).inflate(range, MAX_Y, range);
     }
-
 
     public static boolean isInGravityShaft(Player player) {
         return isInAntiGrav(player, player.getBoundingBox().inflate(8, MAX_Y, 8), player.level());
     }
 
-
     static double easeMovement() {
-        double smoothedMovement = Math.abs(GravityUtil.ACCELERATION);
+        double smoothedMovement = Math.abs(ACCELERATION);
         return smoothedMovement * smoothedMovement * smoothedMovement;
     }
-
-
-
 }
