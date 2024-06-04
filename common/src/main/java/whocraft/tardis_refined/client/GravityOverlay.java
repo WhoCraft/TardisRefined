89b635573104af1c9fc6386fd17afe51d30d5877
Craig <sandedshoes@gmail.com>
2024-06-04 23:24:48 +0100

    FPS Fix

--- common/src/main/java/whocraft/tardis_refined/client/GravityOverlay.java
+++ common/src/main/java/whocraft/tardis_refined/client/GravityOverlay.java
@@ -6,21 +6,35 @@ import com.mojang.math.Transformation;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.world.entity.player.Player;
 import whocraft.tardis_refined.common.GravityUtil;
 import whocraft.tardis_refined.constants.ModMessages;
 
 public class GravityOverlay {
 
+    private static boolean isInShaft = false;
+
+    private static void checkOverlay(Player player){
+        isInShaft = GravityUtil.isInGravityShaft(player);
+    }
+
     public static void renderOverlay(PoseStack poseStack) {
         Minecraft mc = Minecraft.getInstance();
         Font fontRenderer = mc.font;
+        LocalPlayer player = mc.player;
 
-        if (GravityUtil.isInGravityShaft(mc.player) && !mc.options.hideGui) {
+        if(player.tickCount % 100 == 0){
+            checkOverlay(player);
+        }
+
+        if (isInShaft && !mc.getDebugOverlay().showDebugScreen()) {
             poseStack.pushPose();
             poseStack.scale(1.2f, 1.2f, 1.2f);
+
 
             int x = 5;
             int y = 5;
