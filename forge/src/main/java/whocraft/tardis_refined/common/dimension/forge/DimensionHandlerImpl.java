7aa5689a5c1604c0c8e6ff0e9504fd7f1dc1e9f1
Craig <sandedshoes@gmail.com>
2024-08-19 19:23:18 +0100

    Fixed Forge package mistake from cherry pick

--- forge/src/main/java/whocraft/tardis_refined/common/dimension/forge/DimensionHandlerImpl.java
+++ forge/src/main/java/whocraft/tardis_refined/common/dimension/forge/DimensionHandlerImpl.java
@@ -1,4 +1,4 @@
-package whocraft.tardis_refined.common.dimension.neoforge;
+package whocraft.tardis_refined.common.dimension.forge;
 
 import com.google.common.collect.ImmutableList;
 import com.google.gson.JsonObject;
