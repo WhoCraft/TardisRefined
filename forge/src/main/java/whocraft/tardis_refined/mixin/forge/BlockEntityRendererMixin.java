8ed2a5313a786b384a6e0cb5f0741c509eb16322
Craig <sandedshoes@gmail.com>
2024-09-06 10:42:57 +0100

    Bulkheads no longer stop rendering when looking at certain angles

---
+++ forge/src/main/java/whocraft/tardis_refined/mixin/forge/BlockEntityRendererMixin.java
@@ -0,0 +1,27 @@
+package whocraft.tardis_refined.mixin.forge;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import net.minecraftforge.common.extensions.IForgeBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import whocraft.tardis_refined.common.blockentity.console.GlobalConsoleBlockEntity;
+import whocraft.tardis_refined.common.blockentity.device.ConsoleConfigurationBlockEntity;
+import whocraft.tardis_refined.common.blockentity.door.BulkHeadDoorBlockEntity;
+import whocraft.tardis_refined.common.blockentity.door.RootShellDoorBlockEntity;
+import whocraft.tardis_refined.common.blockentity.shell.GlobalShellBlockEntity;
+
+@Mixin({ConsoleConfigurationBlockEntity.class, GlobalShellBlockEntity.class, RootShellDoorBlockEntity.class, GlobalShellBlockEntity.class, GlobalConsoleBlockEntity.class, BulkHeadDoorBlockEntity.class})
+public class BlockEntityRendererMixin extends BlockEntity implements IForgeBlockEntity {
+
+    public BlockEntityRendererMixin(BlockEntityType<?> arg, BlockPos arg2, BlockState arg3) {
+        super(arg, arg2, arg3);
+    }
+
+    @Override
+    public AABB getRenderBoundingBox() {
+        return INFINITE_EXTENT_AABB;
+    }
+}
