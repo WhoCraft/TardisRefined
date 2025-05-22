5d5527fc86be43db9087048d1d24cd3951c2d3fa
Auroali <dawn.auroali@gmail.com>
2025-05-22 17:56:49 -0300

    fix the tardis forgetting its dimension on reload

--- common/src/main/java/whocraft/tardis_refined/common/tardis/TardisNavLocation.java
+++ common/src/main/java/whocraft/tardis_refined/common/tardis/TardisNavLocation.java
@@ -67,6 +67,8 @@ public class TardisNavLocation {
 
         if (tag.contains("name"))
             loc.setName(tag.getString("name"));
+
+        loc.level = DimensionUtil.getLevel(loc.dimensionKey);
         return loc;
     }
 
