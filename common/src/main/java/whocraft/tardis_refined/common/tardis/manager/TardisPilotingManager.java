378199efdae8e8c0b7b34af4760861b1eaa6169c
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-02-28 00:12:29 +0100

    Fix takeoff sound pos.

--- common/src/main/java/whocraft/tardis_refined/common/tardis/manager/TardisPilotingManager.java
+++ common/src/main/java/whocraft/tardis_refined/common/tardis/manager/TardisPilotingManager.java
@@ -720,9 +720,7 @@ public class TardisPilotingManager extends TickableHandler {
 
 
             operator.setDoorClosed(true);
-            if (operator.getInternalDoor() != null) {
-                operator.getLevel().playSound(null, operator.getInternalDoor().getDoorPosition(), TRSoundRegistry.TARDIS_TAKEOFF.get(), SoundSource.AMBIENT, 10f, 1f);
-            }
+            operator.getLevel().playSound(null, currentConsoleBlockPos, TRSoundRegistry.TARDIS_TAKEOFF.get(), SoundSource.AMBIENT, 10f, 1f);
             operator.getExteriorManager().playSoundAtShell(TRSoundRegistry.TARDIS_TAKEOFF.get(), SoundSource.BLOCKS, 1, 1);
 
 
