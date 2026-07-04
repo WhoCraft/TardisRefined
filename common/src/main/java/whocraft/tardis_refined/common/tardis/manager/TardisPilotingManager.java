c0f6393ad102bd4a75cd9d1e6adf1479bbc75e34
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-07-04 22:53:06 +0200

    Stop flight events when throttle at 0% and try to ensure they don't turn back on when destination already reached.

--- common/src/main/java/whocraft/tardis_refined/common/tardis/manager/TardisPilotingManager.java
+++ common/src/main/java/whocraft/tardis_refined/common/tardis/manager/TardisPilotingManager.java
@@ -231,6 +231,19 @@ public class TardisPilotingManager extends TickableHandler {
     }
 
     private void onFlightTick(ServerLevel level) {
+        if (!this.autoLand) {
+            var flightDance = operator.getFlightDanceManager();
+            if (this.throttleStage == 0) {
+                if (flightDance.isDancing()) {
+                    flightDance.stopDancing();
+                }
+            } else {
+                if (!flightDance.isDancing() && isInFlight() && !isLanding() && distanceCovered < flightDistance) {
+                    flightDance.startFlightDance(currentConsole);
+                }
+            }
+        }
+
         if (this.throttleStage != 0 || this.autoLand) {
             ticksInFlight++;
 
@@ -967,7 +980,7 @@ public class TardisPilotingManager extends TickableHandler {
         this.operator.getExteriorManager().setIsTakingOff(false);
         TardisCommonEvents.TAKE_OFF.invoker().onTakeOff(operator, lastKnown.getLevel(), lastKnown.getPosition());
 
-        if (this.currentConsole != null) {
+        if (this.currentConsole != null && distanceCovered < flightDistance) {
             operator.getFlightDanceManager().startFlightDance(this.currentConsole);
         }
 
@@ -1051,7 +1064,7 @@ public class TardisPilotingManager extends TickableHandler {
         if (this.flightDistance == 0) {
             return 0;
         }
-        return (float) this.distanceCovered / this.flightDistance;
+        return Mth.clamp((float) this.distanceCovered / this.flightDistance, 0, 1);
     }
 
     public void offsetTargetPositionX(int x) {
