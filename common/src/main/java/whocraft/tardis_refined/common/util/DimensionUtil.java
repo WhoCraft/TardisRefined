8c6d0340819b348aab95ea7d267428ab2d53f59c
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-07-07 12:46:09 +0200

    Add dynamic matchers to banned dimensions and adventure mode defaults.

--- common/src/main/java/whocraft/tardis_refined/common/util/DimensionUtil.java
+++ common/src/main/java/whocraft/tardis_refined/common/util/DimensionUtil.java
@@ -9,15 +9,40 @@ import whocraft.tardis_refined.TRConfig;
 import whocraft.tardis_refined.TardisRefined;
 import whocraft.tardis_refined.registry.TRDimensionTypes;
 
+import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
 
 public class DimensionUtil {
 
+    private static final String REGEX_PREFIX = "[regex]";
+    private static final String SUBSTRING_PREFIX = "[substring]";
+    private static final String NAMESPACE_PREFIX = "[namespace]";
+
+    public static Predicate<ResourceKey<Level>> getDimensionPredicate(String configEntry) {
+        if (configEntry.startsWith(REGEX_PREFIX)) {
+            String regex = configEntry.substring(REGEX_PREFIX.length());
+            return dim -> dim.location().toString().matches(regex);
+        }
+        if (configEntry.startsWith(SUBSTRING_PREFIX)) {
+            String substring = configEntry.substring(SUBSTRING_PREFIX.length());
+            return dim -> dim.location().toString().contains(substring);
+        }
+        if (configEntry.startsWith(NAMESPACE_PREFIX)) {
+            String namespace = configEntry.substring(NAMESPACE_PREFIX.length());
+            return dim -> dim.location().getNamespace().equals(namespace);
+        }
+        return dim -> dim.location().toString().equals(configEntry);
+    }
+
+    public static Predicate<ResourceKey<Level>> getDimensionPredicate(List<? extends String> configEntries) {
+        return configEntries.stream().map(DimensionUtil::getDimensionPredicate).reduce(Predicate::or).orElse(dim -> false);
+    }
+
     public static boolean isAllowedDimension(ResourceKey<Level> level) {
         String namespace = level.location().getNamespace();
-        String location = level.location().toString();
-        var bannedDimensions = TRConfig.SERVER.BANNED_DIMENSIONS.get();
-        return !namespace.contains(TardisRefined.MODID) && !bannedDimensions.contains(location);
+        var bannedDimensions = getDimensionPredicate(TRConfig.SERVER.BANNED_DIMENSIONS.get());
+        return !namespace.contains(TardisRefined.MODID) && !bannedDimensions.test(level);
     }
     public static Set<ResourceKey<Level>> getTardisLevels(MinecraftServer server) {
         Set<ResourceKey<Level>> set = Sets.newHashSet();
