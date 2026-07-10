736c4ad25a61203d5f7b8c0976707eaad73c90c0
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-07-10 14:26:34 +0200

    I guess we need a dummy.

--- common/src/main/java/whocraft/tardis_refined/compat/SublevelPositionRegistry.java
+++ common/src/main/java/whocraft/tardis_refined/compat/SublevelPositionRegistry.java
@@ -2,12 +2,16 @@ package whocraft.tardis_refined.compat;
 
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.Registry;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import whocraft.tardis_refined.TardisRefined;
 import whocraft.tardis_refined.compat.sable.SableSublevelAccessor;
 import whocraft.tardis_refined.registry.DeferredRegister;
 import whocraft.tardis_refined.registry.RegistryBuilder;
+
+import java.util.Optional;
 
 public class SublevelPositionRegistry {
     
@@ -18,8 +22,31 @@ public class SublevelPositionRegistry {
     ).build();
 
     public static final DeferredRegister<MapCodec<? extends SublevelAccessor.LoadablePositionReference>> DEFERRED = DeferredRegister.create(TardisRefined.MODID, KEY);
-    
+
+    private static class Dummy implements SublevelAccessor.LoadablePositionReference {
+
+        private static final MapCodec<Dummy> CODEC = MapCodec.unit(new Dummy());
+
+        private Dummy() {}
+
+        @Override
+        public Optional<SublevelAccessor.PositionReference> tryLoad(MinecraftServer server) {
+            return Optional.empty();
+        }
+
+        @Override
+        public Optional<Component> metadata() {
+            return Optional.empty();
+        }
+
+        @Override
+        public MapCodec<? extends SublevelAccessor.LoadablePositionReference> codec() {
+            return CODEC;
+        }
+    }
+
     static {
+        DEFERRED.register("dummy", () -> Dummy.CODEC); // Needed because game crashes when registry is empty.
         if (ModCompatChecker.sable()) {
             DEFERRED.register("sable", () -> SableSublevelAccessor.LoadableSablePosition.CODEC);
         }
