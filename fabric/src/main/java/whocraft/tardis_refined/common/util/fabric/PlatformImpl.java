9ec890d4ad78087df55f39caa852840d56012d2a
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-08-11 20:36:42 +0200

    Allow checking if a specific version of a specific mod is loaded.

--- fabric/src/main/java/whocraft/tardis_refined/common/util/fabric/PlatformImpl.java
+++ fabric/src/main/java/whocraft/tardis_refined/common/util/fabric/PlatformImpl.java
@@ -1,13 +1,18 @@
 package whocraft.tardis_refined.common.util.fabric;
 
+import com.mojang.serialization.DataResult;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.api.Version;
+import net.fabricmc.loader.api.VersionParsingException;
 import net.fabricmc.loader.api.metadata.ModMetadata;
 import net.minecraft.client.Minecraft;
 import net.minecraft.server.MinecraftServer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import whocraft.tardis_refined.common.util.Platform;
 
 import java.util.Collection;
@@ -21,8 +26,26 @@ public class PlatformImpl {
         return !FabricLoader.getInstance().isDevelopmentEnvironment();
     }
 
-    public static boolean isModLoaded(String id) {
-        return FabricLoader.getInstance().isModLoaded(id);
+    public static DataResult<Platform.CommonVersion> parseVersion(String version) {
+        try {
+            return DataResult.success(new FabricVersion(Version.parse(version)));
+        } catch (VersionParsingException e) {
+            return DataResult.error(e::getMessage);
+        }
+    }
+
+    public static boolean isModLoaded(String id, @Nullable Platform.CommonVersionRange range) {
+        if (FabricLoader.getInstance().isModLoaded(id)) {
+            if (range != null) {
+                var container = FabricLoader.getInstance().getModContainer(id);
+                if (container.isEmpty()) return false;
+                var version = new FabricVersion(container.get().getMetadata().getVersion());
+                return range.contains(version);
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static Collection<String> getModIds() {
@@ -67,4 +90,20 @@ public class PlatformImpl {
                 .orElse(namespace);
     }
 
+    public record FabricVersion(Version version) implements Platform.CommonVersion {
+
+        @Override
+        public int compareTo(@NotNull Platform.CommonVersion commonVersion) {
+            if (commonVersion instanceof FabricVersion otherVersion) {
+                return version.compareTo(otherVersion.version);
+            } else {
+                throw new IllegalArgumentException("Tried to compare mismatching version formats");
+            }
+        }
+
+        @Override
+        public String versionString() {
+            return version.getFriendlyString();
+        }
+    }
 }
