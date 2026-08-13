47d5fb6e14feb14ebf1fd3f711662696346ae9c9
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-08-13 18:38:18 +0200

    Fix items dropped in shell view ending up outside the TARDIS.

--- changelog.md
+++ changelog.md
@@ -6,3 +6,4 @@
 Fix players glitching out when relogging while in shell view on Fabric.
 Fix shell-view return position being rounded to the nearest block position.
 Fix player exiting shell-view not facing the correct direction.
+Fix items dropped in shell view ending up outside the TARDIS.
--- common/src/main/java/whocraft/tardis_refined/mixin/PlayerEntityMixin.java
+++ common/src/main/java/whocraft/tardis_refined/mixin/PlayerEntityMixin.java
@@ -1,21 +1,33 @@
 package whocraft.tardis_refined.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.tags.DamageTypeTags;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 import whocraft.tardis_refined.common.GravityClient;
 import whocraft.tardis_refined.common.GravityUtil;
 import whocraft.tardis_refined.common.capability.player.TardisPlayerInfo;
 
 @Mixin(Player.class)
-public class PlayerEntityMixin {
+public abstract class PlayerEntityMixin extends LivingEntity {
+
+    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, Level level) {
+        super(entityType, level);
+    }
 
     @Inject(method = "travel(Lnet/minecraft/world/phys/Vec3;)V", at = @At("TAIL"), cancellable = true)
     private void move(Vec3 vec3, CallbackInfo info) {
@@ -30,6 +42,57 @@ public class PlayerEntityMixin {
 
         if (!player.level().isClientSide) return;
         GravityClient.moveGravity(player, info);
+    }
+
+    @ModifyArgs(
+            method = "drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/item/ItemEntity;<init>(Lnet/minecraft/world/level/Level;DDDLnet/minecraft/world/item/ItemStack;)V"
+            )
+    )
+    public void dropCheckShellView(Args args, @Share("rotation") LocalRef<Vec2> rotation) {
+        TardisPlayerInfo.get((Player) (Object) this).ifPresent(data -> {
+            if (data.isViewingTardis() && getServer() != null) {
+                var prevDim = getServer().getLevel(data.getPlayerPreviousDim());
+                if (prevDim != null) {
+                    args.set(0, prevDim);
+                    var prevPos = data.getPlayerPreviousPosAccurate();
+                    args.set(1, prevPos.x);
+                    args.set(2, prevPos.y + ((double) args.get(2)) - getY());
+                    args.set(3, prevPos.z);
+                    rotation.set(new Vec2(data.getPlayerPreviousRot(), data.getPlayerPreviousYaw()));
+                }
+            }
+        });
+    }
+
+    @ModifyExpressionValue(
+            method = "drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/player/Player;getXRot()F"
+            )
+    )
+    public float dropFixXRot(float original, @Share("rotation") LocalRef<Vec2> rotation) {
+        if (rotation.get() != null) {
+            return rotation.get().x;
+        }
+        return original;
+    }
+
+    @ModifyExpressionValue(
+            method = "drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/player/Player;getYRot()F"
+            )
+    )
+    public float dropFixYRot(float original, @Share("rotation") LocalRef<Vec2> rotation) {
+        if (rotation.get() != null) {
+            return rotation.get().y;
+        }
+        return original;
     }
 
 
