df9878c73757f03300607eafc35b877e505995bb
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-08-18 15:35:28 +0200

    Fix GPS not using the same direction and dimension name formatting as the rest of the mod.

--- common/src/main/java/whocraft/tardis_refined/common/tardis/control/flight/ReadoutControl.java
+++ common/src/main/java/whocraft/tardis_refined/common/tardis/control/flight/ReadoutControl.java
@@ -1,5 +1,6 @@
 package whocraft.tardis_refined.common.tardis.control.flight;
 
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
@@ -7,10 +8,13 @@ import whocraft.tardis_refined.common.capability.tardis.TardisLevelOperator;
 import whocraft.tardis_refined.common.entity.ControlEntity;
 import whocraft.tardis_refined.common.tardis.TardisNavLocation;
 import whocraft.tardis_refined.common.tardis.themes.ConsoleTheme;
+import whocraft.tardis_refined.common.util.MiscHelper;
 import whocraft.tardis_refined.common.util.PlayerUtil;
 import whocraft.tardis_refined.compat.ModCompatChecker;
 import whocraft.tardis_refined.compat.valkyrienskies.VSHelper;
 import whocraft.tardis_refined.constants.ModMessages;
+
+import java.util.Locale;
 
 public class ReadoutControl extends whocraft.tardis_refined.common.tardis.control.Control {
     public ReadoutControl(ResourceLocation id) {
@@ -21,6 +25,10 @@ public class ReadoutControl extends whocraft.tardis_refined.common.tardis.contro
         super(id, langId);
     }
 
+    private static String directionName(Direction direction) {
+        return direction.getName().substring(0, 1).toUpperCase(Locale.ROOT) + direction.getName().substring(1);
+    }
+
     @Override
     public boolean onLeftClick(TardisLevelOperator operator, ConsoleTheme theme, ControlEntity controlEntity, Player player) {
 
@@ -28,7 +36,7 @@ public class ReadoutControl extends whocraft.tardis_refined.common.tardis.contro
         if (ModCompatChecker.valkyrienSkies()) {
             currentPosition = VSHelper.toWorldLocation(currentPosition);
         }
-        PlayerUtil.sendMessage(player, Component.translatable(ModMessages.CURRENT).append(" - X: " + currentPosition.getPosition().getX() + " Y: " + currentPosition.getPosition().getY() + " Z: " + currentPosition.getPosition().getZ() + " F: " + currentPosition.getDirection().getName() + " D: " + currentPosition.getDimensionKey().location().getPath()), true);
+        PlayerUtil.sendMessage(player, Component.translatable(ModMessages.CURRENT).append(" - X: " + currentPosition.getPosition().getX() + " Y: " + currentPosition.getPosition().getY() + " Z: " + currentPosition.getPosition().getZ() + " F: " + directionName(currentPosition.getDirection()) + " D: ").append(MiscHelper.getCleanDimensionName(currentPosition.getDimensionKey())), true);
 
 
         return true;
@@ -41,7 +49,7 @@ public class ReadoutControl extends whocraft.tardis_refined.common.tardis.contro
         if (ModCompatChecker.valkyrienSkies()) {
             targetLocation = VSHelper.toWorldLocation(targetLocation);
         }
-        PlayerUtil.sendMessage(player, Component.translatable(ModMessages.DESTINATION).append(" - X: " + targetLocation.getPosition().getX() + " Y: " + targetLocation.getPosition().getY() + " Z: " + targetLocation.getPosition().getZ() + " F: " + targetLocation.getDirection().getName() + " D: " + targetLocation.getDimensionKey().location().getPath()), true);
+        PlayerUtil.sendMessage(player, Component.translatable(ModMessages.DESTINATION).append(" - X: " + targetLocation.getPosition().getX() + " Y: " + targetLocation.getPosition().getY() + " Z: " + targetLocation.getPosition().getZ() + " F: " + directionName(targetLocation.getDirection()) + " D: ").append(MiscHelper.getCleanDimensionName(targetLocation.getDimensionKey())), true);
 
         return true;
     }
